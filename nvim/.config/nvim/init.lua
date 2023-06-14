ce8c6bb3b1edbc2967d18fe80e753325364dabb9
RamunnoAJ <ramunnoagustin@gmail.com>
2023-06-14 14:07:56 -0300

    add: codeium plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -64,7 +64,7 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
-
+  'Exafunction/codeium.vim',
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
