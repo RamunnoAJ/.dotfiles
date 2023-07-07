8c1605b79fa0fd936f3230758accf1351edb9295
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-07 13:29:25 -0300

    add: null-ls plugin config and autopairs plugins

--- nvim/.config/nvim/lua/custom/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -1,26 +1,27 @@
 local M = {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "mason.nvim" },
+  'jose-elias-alvarez/null-ls.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = { 'mason.nvim' },
   opts = function()
-    local null_ls = require("null-ls")
+    local null_ls = require 'null-ls'
     local formatting = null_ls.builtins.formatting
     local diagnostics = null_ls.builtins.diagnostics
     local code_actions = null_ls.builtins.code_actions
     local completion = null_ls.builtins.completion
     on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        local format_on_save = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+      if client.supports_method 'textDocument/formatting' then
+        local format_on_save = vim.api.nvim_create_augroup('LspFormatting', { clear = true })
         vim.api.nvim_create_autocmd('BufWritePre', {
           group = format_on_save,
           buffer = bufnr,
           callback = function()
-            vim.lsp.buf.format({
-              bufnr = bufnr,
+            vim.lsp.buf.format {
+              async = false,
               filter = function(_client)
-                return _client.name == "null-ls"
-              end
-            })
+                return _client.name == 'null-ls'
+              end,
+            }
+            vim.lsp.buf.formatting_sync()
           end,
         })
       end
@@ -30,13 +31,10 @@ local M = {
         formatting.stylua,
         formatting.prettier,
         formatting.eslint,
-        formatting.fish_indent,
         diagnostics.eslint,
-        diagnostics.fish,
-        diagnostics.tsc,
         code_actions.eslint,
         code_actions.gitsigns,
-        completion.spell
+        completion.spell,
       },
     }
   end,
