0f947fe56ee0dc7e9f03c097f05b1c3b11f85ae0
RamunnoAJ <ramunnoagustin@gmail.com>
2023-10-19 22:59:29 -0300

    change: colorscheme

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -1,7 +1,35 @@
 return {
-  'JoosepAlviste/palenightfall.nvim',
+  --{
+  -- "bluz71/vim-nightfly-guicolors",
+  --priority = 1000, -- make sure to load this before all the other start plugins
+  -- config = function()
+  -- load the colorscheme here
+  --   vim.cmd([[colorscheme nightfly]])
+  -- end,
+  --},
+
+  'navarasu/onedark.nvim',
   priority = 1000,
+
   config = function()
-    vim.cmd.colorscheme 'palenightfall'
+    local onedark = require("onedark")
+    onedark.setup({
+      style = 'cool',
+      term_colors = true,
+      transparent = false,
+      code_style = {
+        comments = 'italic',
+        keywords = 'none',
+        functions = 'none',
+        strings = 'none',
+        variables = 'none'
+      },
+      lualine = {
+        transparent = true
+      }
+    })
+    onedark.load()
+
+    vim.cmd.colorscheme 'onedark'
   end,
 }
