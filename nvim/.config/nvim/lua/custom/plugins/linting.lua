281841c591a15ae20b17353fd45708017aca89ee
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-09 15:32:48 -0300

    remove: unnecessary mappings

--- nvim/.config/nvim/lua/custom/plugins/linting.lua
+++ nvim/.config/nvim/lua/custom/plugins/linting.lua
@@ -22,9 +22,5 @@ return {
         lint.try_lint()
       end
     })
-
-    vim.keymap.set("n", "<leader>L", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file", noremap = true, silent = true })
   end
 }
