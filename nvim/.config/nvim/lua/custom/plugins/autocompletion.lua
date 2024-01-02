9194b4a0bfb3fef1c6e58a094f64dcd29d9faccb
RamunnoAJ <ramunnoagustin@gmail.com>
2024-01-02 14:26:09 -0300

    fix: cmp snippets appearing before keywords

--- nvim/.config/nvim/lua/custom/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/custom/plugins/autocompletion.lua
@@ -1,14 +1,43 @@
 return {
-  'hrsh7th/nvim-cmp',
+  "hrsh7th/nvim-cmp",
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
+    "hrsh7th/cmp-nvim-lsp",
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
-
-    -- Adds LSP completion capabilities
-    'hrsh7th/cmp-nvim-lsp',
-
-    -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets',
   },
+
+  config = function()
+    local cmp = require("cmp")
+    local types = require("cmp.types")
+
+    -- Function to sort LSP snippets, so that they appear at the end of LSP suggestions
+    local function deprioritize_snippet(entry1, entry2)
+      if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then
+        return false
+      end
+      if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then
+        return true
+      end
+    end
+
+    -- Take care of source ordering and group_index
+    cmp.setup({
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          deprioritize_snippet,
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        }
+      }
+    })
+  end
 }
