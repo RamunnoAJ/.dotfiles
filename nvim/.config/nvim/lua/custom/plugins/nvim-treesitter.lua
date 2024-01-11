4245da19a6aff809514ec218ac309e1bc65ef9d0
RamunnoAJ <ramunnoagustin@gmail.com>
2024-01-11 15:38:27 -0300

    change: ' to "

--- nvim/.config/nvim/lua/custom/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
 
     dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
+        "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    build = ':TSUpdate',
+    build = ":TSUpdate",
     config = function()
-        require('nvim-treesitter.configs').setup({
+        require("nvim-treesitter.configs").setup({
             -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = { 'go', 'lua', 'tsx', 'typescript', 'javascript', 'vim', 'html', 'css', 'rust', 'json', 'jsdoc' },
+            ensure_installed = { "lua", "tsx", "typescript", "javascript", "vim", "html", "css", "rust", "json", "jsdoc" },
 
             -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
             auto_install = true,
@@ -18,10 +18,10 @@ return {
             incremental_selection = {
                 enable = true,
                 keymaps = {
-                    init_selection = '<Leader>ss',
-                    node_incremental = '<Leader>si',
-                    scope_incremental = '<Leader>sc',
-                    node_decremental = '<Leader>sd',
+                    init_selection = "<Leader>ss",
+                    node_incremental = "<Leader>si",
+                    scope_incremental = "<Leader>sc",
+                    node_decremental = "<Leader>sd",
                 },
             },
             textobjects = {
@@ -30,32 +30,32 @@ return {
                     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
                     keymaps = {
                         -- You can use the capture groups defined in textobjects.scm
-                        ['aa'] = '@parameter.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
                     },
                 },
                 move = {
                     enable = true,
                     set_jumps = true, -- whether to set jumps in the jumplist
                     goto_next_start = {
-                        [']m'] = '@function.outer',
-                        [']]'] = '@class.outer',
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
                     },
                     goto_next_end = {
-                        [']M'] = '@function.outer',
-                        [']['] = '@class.outer',
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
                     },
                     goto_previous_start = {
-                        ['[m'] = '@function.outer',
-                        ['[['] = '@class.outer',
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
                     },
                     goto_previous_end = {
-                        ['[M'] = '@function.outer',
-                        ['[]'] = '@class.outer',
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
                     },
                 },
                 swap = {
