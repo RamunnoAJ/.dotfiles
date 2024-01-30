a447890110a42ab755a837bb2902f64ed599a5bc
RamunnoAJ <ramunnoagustin@gmail.com>
2024-01-30 10:12:10 -0300

    remap: harpoon keymaps

--- nvim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -14,7 +14,19 @@ return {
             }
         })
 
-        vim.keymap.set("n", "<M-0>",
+        harpoon:extend({
+            UI_CREATE = function(cx)
+                vim.keymap.set("n", "<C-v>", function()
+                    harpoon.ui:select_menu_item({ vsplit = true })
+                end, { buffer = cx.bufnr })
+
+                vim.keymap.set("n", "<C-x>", function()
+                    harpoon.ui:select_menu_item({ split = true })
+                end, { buffer = cx.bufnr })
+            end
+        })
+
+        vim.keymap.set("n", "<M-e>",
             function()
                 harpoon.ui:toggle_quick_menu(harpoon:list(),
                     { border = "rounded", ui_width_ratio = 0.3, ui_fallback_width = 0.25 })
