00d3a717afc6724152e981a92c569ea0b3fd45e6
RamunnoAJ <ramunnoagustin@gmail.com>
2024-06-13 20:00:03 -0300

    feat: modularize cmp

---
+++ nvim/.config/nvim/lua/custom/plugins/completion.lua
@@ -0,0 +1,65 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip"
+    },
+
+    config = function()
+        local cmp = require("cmp")
+        local cmp_types = require("cmp.types")
+
+        local function deprioritize_snippet(entry1, entry2)
+            if entry1:get_kind() == cmp_types.lsp.CompletionItemKind.Snippet then
+                return false
+            end
+            if entry2:get_kind() == cmp_types.lsp.CompletionItemKind.Snippet then
+                return true
+            end
+        end
+
+        local luasnip = require("luasnip")
+        require("luasnip.loaders.from_vscode").lazy_load()
+        luasnip.config.setup({})
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end
+            },
+            sorting = {
+                priority_weight = 2,
+                comparators = {
+                    deprioritize_snippet,
+                    cmp.config.compare.offset,
+                    cmp.config.compare.exact,
+                    cmp.config.compare.score,
+                    cmp.config.compare.recently_used,
+                    cmp.config.compare.locality,
+                    cmp.config.compare.kind,
+                    cmp.config.compare.sort_text,
+                    cmp.config.compare.length,
+                    cmp.config.compare.order,
+                }
+            },
+            mapping = cmp.mapping.preset.insert {
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-_>"] = cmp.mapping.complete {},
+                ["<CR>"] = cmp.mapping.confirm {
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                }
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "luasnip" }
+            },
+        })
+    end
+
+}
--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -3,28 +3,15 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/nvim-cmp",
     },
 
     config = function()
-        local cmp = require("cmp")
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
             vim.lsp.protocol.make_client_capabilities(),
             cmp_lsp.default_capabilities())
-
-        local cmp_types = require("cmp.types")
-        local function deprioritize_snippet(entry1, entry2)
-            if entry1:get_kind() == cmp_types.lsp.CompletionItemKind.Snippet then
-                return false
-            end
-            if entry2:get_kind() == cmp_types.lsp.CompletionItemKind.Snippet then
-                return true
-            end
-        end
 
         require("fidget").setup({})
         require("mason").setup()
@@ -33,7 +20,8 @@ return {
                 "rust_analyzer",
                 "tsserver",
                 "lua_ls",
-                "clangd"
+                "clangd",
+                "gopls"
             },
 
             handlers = {
@@ -55,39 +43,6 @@ return {
                         }
                     })
                 end
-            },
-        })
-
-
-        cmp.setup({
-            sorting = {
-                priority_weight = 2,
-                comparators = {
-                    deprioritize_snippet,
-                    cmp.config.compare.offset,
-                    cmp.config.compare.exact,
-                    cmp.config.compare.score,
-                    cmp.config.compare.recently_used,
-                    cmp.config.compare.locality,
-                    cmp.config.compare.kind,
-                    cmp.config.compare.sort_text,
-                    cmp.config.compare.length,
-                    cmp.config.compare.order,
-                }
-            },
-            mapping = cmp.mapping.preset.insert {
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-_>"] = cmp.mapping.complete {},
-                ["<CR>"] = cmp.mapping.confirm {
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }
-            },
-            sources = {
-                { name = "nvim_lsp" },
             },
         })
 
