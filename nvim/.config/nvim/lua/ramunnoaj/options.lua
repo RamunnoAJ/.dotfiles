5bff468ea7ada4a1a3bd357b498fe1638f31828b
RamunnoAJ <ramunnoagustin@gmail.com>
2024-06-26 18:06:21 -0300

    update: config

--- nvim/.config/nvim/lua/ramunnoaj/options.lua
+++ nvim/.config/nvim/lua/ramunnoaj/options.lua
@@ -157,8 +157,8 @@ autocmd({ "BufLeave", "ExitPre" }, {
 })
 
 vim.g.netrw_banner = 0
-vim.g.netrw_winsize = -28
-vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 25
+vim.g.netrw_liststyle = 0
 vim.g.netrw_list_hide = "^./$"
 vim.g.netrw_browse_split = 0
 vim.g.netrw_browsex_viewer = "open -a chrome"
