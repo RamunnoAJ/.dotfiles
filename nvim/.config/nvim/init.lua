c372a654ec009cf04d699a3c2e3fcd07a569f32a
RamunnoAJ <ramunnoagustin@gmail.com>
2025-08-16 16:42:48 -0300

    feat: autoformat with lsp on save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -43,7 +43,7 @@ vim.pack.add({
 	{ src = "https://github.com/j-hui/fidget.nvim" },
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	{ src = "https://github.com/Saghen/blink.cmp", version = "1.*" },
+	{ src = "https://github.com/Saghen/blink.cmp",               version = "1.*" },
 })
 
 require "mason".setup()
@@ -98,6 +98,22 @@ local map = vim.keymap.set
 local augroup = vim.api.nvim_create_augroup
 local RamunnoGroup = augroup('Ramunno', {})
 local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if not client then return end
+
+		if client:supports_method('textDocument/formatting') then
+			autocmd('BufWritePre', {
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+				end
+			})
+		end
+	end
+})
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 autocmd('TextYankPost', {
