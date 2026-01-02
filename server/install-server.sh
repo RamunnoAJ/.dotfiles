22c1e71538363bdd99369a48b3e10c3129437968
RamunnoAJ <ramunnoagustin@gmail.com>
2026-01-02 13:24:04 -0300

    fix(server): update neovim release asset name to nvim-linux-x86_64

--- server/install-server.sh
+++ server/install-server.sh
@@ -21,15 +21,15 @@ fi
 
 # 1.1 Install Latest Neovim (from source/release, to avoid old apt versions)
 echo ">>> Installing latest Neovim..."
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 # Create a symbolic link so 'nvim' is in the path
 if [ -L /usr/local/bin/nvim ]; then
     sudo rm /usr/local/bin/nvim
 fi
-sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
-rm nvim-linux64.tar.gz
+sudo ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+rm nvim-linux-x86_64.tar.gz
 
 # 2. Setup Zsh
 echo ">>> Setting up Zsh..."
